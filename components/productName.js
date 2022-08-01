c54a4a3bbb31a6de81f7e8e5986bfcad2547e310
KlrD13 <claire.dambrin@hotmail.fr>
2022-08-01 17:57:48 +0200

    commit Julien

---
+++ components/productName.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { useFonts } from "expo-font";
+
+const productName = (product) => {
+  useFonts({
+    Dosis: require("../assets/fonts/Dosis.ttf"),
+  });
+  return (
+    <View style={styles.tag}>
+      <Text style={styles.name}>{product.name}</Text>
+      <Text style={styles.species}>{product.species}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  name: {
+    fontFamily: "Dosis",
+    fontWeight: "bold",
+  },
+  species: {
+    fontFamily: "Dosis",
+    fontWeight: "light",
+  },
+});
+
+module.exports = productName;
--- screens/GardenScreen.js
+++ screens/GardenScreen.js
@@ -1,5 +1,24 @@
-import { View, Text  } from 'react-native';
+const { useState, useEffect } = require("react");
+const { View } = require("react-native");
+const productName = require("../components/productName");
 
-export default function GardenScreen() {
-    return <View style={{ flex: 1, backgroundColor: "#FFFFFF" }}><Text>Garden Screen</Text></View>;
+function GardenScreen() {
+  [productList, setProductList] = useState([]);
+
+  useEffect(async (product) => {
+    const load = async () => {
+      const product_id = "62e7d11b98ac631e4375fc58";
+      const loadProductList = await fetch("http://10.2.1.56:3000/product%22");
+      const response = loadProductList.json;
+      setProductList(response);
+    };
+  }, []);
+
+  const product_id = "62e7d11b98ac631e4375fc58";
+
+  const CardList = productList.map((product) => {
+    return productName(product);
+  });
+
+  return <View>{CardList}</View>;
 }
