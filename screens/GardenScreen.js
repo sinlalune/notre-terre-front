27552a09a19f1bdba911b4b86a482e611e476702
KlrD13 <claire.dambrin@hotmail.fr>
2022-08-05 10:34:42 +0200

    variable env maj

--- screens/GardenScreen.js
+++ screens/GardenScreen.js
@@ -8,64 +8,64 @@ const AddIcon = require("../components/AddIcon");
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import { Shadow } from "react-native-shadow-2";
 
+import { API_BACKEND } from "@env";
+
 export default function GardenScreen() {
-  const [productList, setProductList] = useState([]);
+	const [productList, setProductList] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      console.log("garden started");
-      const loadProductList = await fetch(
-        `https://${REACT_APP_API_BACKEND}/card/productlist`
-      );
-      const response = await loadProductList.json();
-      console.log("response", response.product);
+	useEffect(() => {
+		(async () => {
+			console.log("garden started");
+			const loadProductList = await fetch(`${API_BACKEND}/card/productlist`);
+			const response = await loadProductList.json();
+			console.log("response", response.product);
 
-      setProductList(response.product);
-    })();
-  }, []);
+			setProductList(response.product);
+		})();
+	}, []);
 
-  console.log("liste", productList);
+	console.log("liste", productList);
 
-  const CardList = productList.map((product, i) => {
-    return (
-      <ProductCard
-        key={i}
-        name={product.name}
-        species={product.species_name}
-        label={product.label}
-        kilo_price={product.kilo_price}
-        date_harvest={product.date_harvest}
-        producer={product.producer}
-        domain_name={product.domain_name}
-        domain_adress={product.domain_adress}
-        product_id={product._id}
-      />
-    );
-  });
+	const CardList = productList.map((product, i) => {
+		return (
+			<ProductCard
+				key={i}
+				name={product.name}
+				species={product.species_name}
+				label={product.label}
+				kilo_price={product.kilo_price}
+				date_harvest={product.date_harvest}
+				producer={product.producer}
+				domain_name={product.domain_name}
+				domain_adress={product.domain_adress}
+				product_id={product._id}
+			/>
+		);
+	});
 
-  console.log("cardlist", CardList);
+	console.log("cardlist", CardList);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        flexWrap: "wrap",
-        backgroundColor: "white",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <View
-          style={{
-            flexDirection: "row",
-            flexWrap: "wrap",
-            width: "100%",
-          }}
-        >
-          {CardList}
-        </View>
-      </View>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				flexWrap: "wrap",
+				backgroundColor: "white",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<View>
+				<View
+					style={{
+						flexDirection: "row",
+						flexWrap: "wrap",
+						width: "100%",
+					}}
+				>
+					{CardList}
+				</View>
+			</View>
+		</View>
+	);
 }
--- screens/ResearchScreen.js
+++ screens/ResearchScreen.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
+import { API_BACKEND } from "@env";
+
 //Import of MapView and Marker
 import MapView, { Marker } from "react-native-maps";
 
@@ -20,99 +22,98 @@ import { Ionicons } from "@expo/vector-icons";
 
 const ProductCard = require("../components/ProductCard");
 
-
 // ASKS PERMISSIONS FOR LOCALISATION
 export default function ResearchScreen() {
-  const [currentLatitude, setCurrentLatitude] = useState(0);
-  const [currentLongitude, setCurrentLongitude] = useState(0);
+	const [currentLatitude, setCurrentLatitude] = useState(0);
+	const [currentLongitude, setCurrentLongitude] = useState(0);
 
-  useEffect(() => {
-    async function askPermissions() {
-      let { status } = await Permissions.askAsync(Permissions.LOCATION);
-      if (status === "granted") {
-        Location.watchPositionAsync({ distanceInterval: 2 }, (location) => {
-          setCurrentLatitude(location.coords.latitude);
-          setCurrentLongitude(location.coords.longitude);
-        });
-      }
-    }
-    askPermissions();
-  }, []);
+	useEffect(() => {
+		async function askPermissions() {
+			let { status } = await Permissions.askAsync(Permissions.LOCATION);
+			if (status === "granted") {
+				Location.watchPositionAsync({ distanceInterval: 2 }, (location) => {
+					setCurrentLatitude(location.coords.latitude);
+					setCurrentLongitude(location.coords.longitude);
+				});
+			}
+		}
+		askPermissions();
+	}, []);
 
-  console.log("This is the user position", currentLatitude, currentLongitude);
+	console.log("This is the user position", currentLatitude, currentLongitude);
 
+	const [productList, setProductList] = useState([]);
 
-  const [productList, setProductList] = useState([]);
+	useEffect(() => {
+		(async () => {
+			console.log("🤖 research page started");
+			const loadProductList = await fetch(`${API_BACKEND}/card/productList`);
+			const response = await loadProductList.json();
+			console.log("response", response.product);
 
-  useEffect(() => {
-    (async () => {
-      console.log("🤖 research page started");
-      const loadProductList = await fetch("http://10.2.1.56:3000/product");
-      const response = await loadProductList.json();
-      console.log("response", response.product);
+			setProductList(response.product);
+		})();
+	}, []);
 
-      setProductList(response.product);
-    })();
-  }, []);
+	console.log("liste", productList);
 
-  console.log("liste", productList);
+	const CardList = productList.map((product, i) => {
+		return (
+			<ProductCard
+				key={i}
+				name={product.name}
+				species={product.species_name}
+				label={product.label}
+				price={product.kilo_price}
+				domain={product.domain_adress}
+			/>
+		);
+	});
 
-  const CardList = productList.map((product, i) => {
-    return (
-      <ProductCard
-        key={i}
-        name={product.name}
-        species={product.species_name}
-        label={product.label}
-        price = {product.kilo_price}
-        domain={product.domain_adress}
-      />
-    );
-  });
+	console.log("cardlist", CardList);
 
-  console.log("cardlist", CardList);
+	const [listPOI, setListPoi] = useState([]);
 
+	var markerPOI = listPOI.map((POI, i) => {
+		return (
+			<Marker
+				key={i}
+				pinColor="blue"
+				coordinate={{ latitude: POI.latitude, longitude: POI.longitude }}
+				title={POI.titre}
+				description={POI.description}
+			/>
+		);
+	});
 
-  const [listPOI, setListPoi] = useState([]);
+	return (
+		<>
+			<Input
+				containerStyle={{ marginBottom: 10, width: "70%" }}
+				inputStyle={{ marginLeft: 10 }}
+				placeholder="Fraise, Paris ..."
+			/>
 
-  var markerPOI = listPOI.map((POI, i) => {
-    return <Marker key={i} pinColor="blue" coordinate={{ latitude: POI.latitude, longitude: POI.longitude }}
-      title={POI.titre}
-      description={POI.description}
-    />
-  });
-  
-
-  return (
-    <>
-      <Input
-        containerStyle={{ marginBottom: 10, width: "70%" }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder="Fraise, Paris ..."
-      />
-
-      <MapView
-        style={{ flex: 1, marginLeft: 10, marginRight: 10 }}
-        region={{
-          latitude: currentLatitude,
-          longitude: currentLongitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: currentLatitude,
-            longitude: currentLongitude,
-          }}
-          title="Your Position"
-          description="You are here"
-        />
-        {markerPOI}
-      </MapView>
-      <ScrollView style={{ marginTop: 10, flex: 3 }}>
-        {CardList}
-      </ScrollView>
-    </>
-  );
+			<MapView
+				style={{ flex: 1, marginLeft: 10, marginRight: 10 }}
+				region={{
+					latitude: currentLatitude,
+					longitude: currentLongitude,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			>
+				<Marker
+					coordinate={{
+						latitude: currentLatitude,
+						longitude: currentLongitude,
+					}}
+					title="Your Position"
+					description="You are here"
+				/>
+				{markerPOI}
+			</MapView>
+			<ScrollView style={{ marginTop: 10, flex: 3 }}>{CardList}</ScrollView>
+		</>
+	);
 }
